86c584a05c8da0f31ee4544e0bbdefd3efdf84da
Holger Roth <hroth@nvidia.com>
2022-08-31 19:32:06 -0400

    update eval script for standalone testing
    Signed-off-by: Holger Roth <hroth@nvidia.com>

--- federated_learning/breast_density_challenge/run_docker_inference.sh
+++ federated_learning/breast_density_challenge/run_docker_inference.sh
@@ -4,7 +4,7 @@ DOCKER_IMAGE=monai-nvflare:latest
 GPU=0
 
 DATA_DIR="${PWD}/data"
-CODE_DIR="${PWD}/code"  # includes inference code
+EVAL_CODE_DIR="${PWD}"  # includes inference code
 TRAIN_RESULT_DIR="${PWD}/result_server"  # participants result folder
 OUTPUT_DIR="${PWD}/test_prediction"  # where to save predictions
 
@@ -16,17 +16,16 @@ OUTPUT_DIR="${PWD}/test_prediction"  # where to save predictions
 # gt1-3 need updating with the right ground truth.
 
 COMMAND="echo INFERENCE:; \
-  python3 pt/utils/mammo_inference.py \
+  python3 /code/pt/utils/mammo_inference.py \
     --model_filepath /result_server/run_1/app_server/best_FL_global_model.pt \
     --dataset_root /data/preprocessed \
     --datalist_prefix /data/dataset_blinded_ \
     --output_root /output; \
   echo EVALUATION:; \
-  python3 pt/utils/challenge_evaluate.py \
+  python3 /eval_code/challenge_evaluate.py \
     --pred /output/test_predictions.json --test_name test_site \
-    --gt1 ../../../dmist_files/dataset_site-1.json \
-    --gt2 ../../../dmist_files/dataset_site-2.json \
-    --gt3 ../../../dmist_files/dataset_site-3.json"
+    --site_names test_site \
+    --gt1 ../../../dmist_files/dataset_test_site.json"
 
 echo "Starting $DOCKER_IMAGE with GPU=${GPU}"
 docker run -it \
@@ -34,7 +33,7 @@ docker run -it \
 --name="mammo_inference" \
 -e NVIDIA_VISIBLE_DEVICES=${GPU} \
 -v ${DATA_DIR}:/data:ro \
--v ${CODE_DIR}:/code:ro \
+-v ${EVAL_CODE_DIR}:/eval_code:ro \
 -v ${TRAIN_RESULT_DIR}:/result_server:ro \
 -v ${OUTPUT_DIR}:/output \
 -w /code \
